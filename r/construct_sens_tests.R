84a67cf6bea324752cb5904bf2491f7d63e237fe
SaiTheinThanTun <theinthantun.sai@gmail.com>
2017-02-08 11:28:10 +0700

    original malmod script reconstructed

--- r/construct_sens_tests.R
+++ r/construct_sens_tests.R
@@ -1,1 +1,9 @@
 #sensitivity analysis constructor
+
+#input: model text, var name, range (or a vector input), output name (eg. model output or incidence etc)
+#output: modified model text to run the model with a loop with varying values (within a range or a vector input)
+        # for the variable of interest
+
+consens <- function(model,v,range,output){
+  
+}
